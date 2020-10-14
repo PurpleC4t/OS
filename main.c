2566c7e750de3285c33c7977dbda92f307f3af11
Minh Đức Cao <34432166+DucMinhCao@users.noreply.github.com>
2020-10-14 23:12:28 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,30 @@
 #include <stdlib.h>
 #include "readline.h"
 #include "findsubstr.h"
+#pragma warning(disable:4996)
 
-
-int main (int argc, char *argv[]) {
-	char *inp;
-	inp = (char*) malloc(100 * sizeof(char));
+int main(int argc, char* argv[]) {
+	char* inp;
+	int stop;
+	inp = (char*)malloc(100 * sizeof(char));
 	char inp1[100];
 	printf("Enter the input string: ");
 	scanf("%s", inp1);
+	fflush(stdin);
 	inp = inp1;
-	printf("The input string: %s\n", inp);
-	char *str;
-	str = (char*) malloc(100 * sizeof(char));
-	int stop = read_line(str);
-	printf("%d\n", stop);
+	printf("The input string: %s", inp);
+	fflush(stdin);
+	printf("\n");
+	char* str;
+	str = (char*)malloc(100 * sizeof(char));
+	do {
+		stop = read_line(str);
+		int a = find_sub_string(str, inp);
+		fflush(stdin);
+		if (a != -1) {
+			printf("%s", str);
+		}
+
+	} while (stop != -1);
+	return 0;
 }
